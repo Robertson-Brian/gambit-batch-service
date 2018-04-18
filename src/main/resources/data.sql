e30675716ae592a457d9d3200e6daad155320904
Yuri Felicio <yfelicio12@gmail.com>
2018-04-18 16:21:48 -0400

    created insert statements for h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+/*******************************************************************************
+*/
+INSERT INTO BATCH VALUES (12,22, 'casmoon', DATE '2015-12-17', 'reston', 10, 'asdsas', DATE '2015-12-17', 1, 'asdsacas', 'dasdasdcsa');
