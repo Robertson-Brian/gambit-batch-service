ff90ec21923223086cfb55f8d033d18847d35857
Peter Farber <peterfarber@fullsail.edu>
2018-04-19 11:15:28 -0400

    Schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE batch
 (
-    batch_id NUMBER PRIMARY KEY,
-    cotrainer_id NUMBER,
+    batch_id INT PRIMARY KEY,
+    cotrainer_id INT,
     end_date TIMESTAMP NULL,
     location VARCHAR(200),
-    resource_id NUMBER,
+    resource_id INT,
     skill_type VARCHAR(200),
     start_date TIMESTAMP NULL,
-    trainer_id NUMBER,
+    trainer_id INT,
     training_name VARCHAR2(200),
     training_type VARCHAR2(200),
 );
+
+CREATE SEQUENCE BATCH_ID_SEQ START WITH 5 INCREMENT BY 5;
