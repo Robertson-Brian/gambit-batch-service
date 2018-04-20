1c94d06050a1f296aac4b8953b53bafe0ff447b2
Peter Farber <peterfarber@fullsail.edu>
2018-04-20 13:17:06 -0400

    Modified pom

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -20,10 +20,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import com.revature.gambit.services.BatchService;
 import com.revature.gambit.services.BatchServiceImpl;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 //@EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class Application {
 	
 	@Autowired
@@ -32,10 +37,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2).select()
-//				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
-//	}
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
+	}
 
 }
