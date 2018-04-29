6b2cfa1356c4ec8b32e2269f0d3d7ce31e043101
palagnajr <peter.alagna@revature.com>
2018-04-28 20:02:48 -0400

    Added custom SwaggerController to solve strip-prefix issue.

--- src/main/java/com/revature/gambit/Application.java
+++ src/main/java/com/revature/gambit/Application.java
@@ -1,22 +1,10 @@
 package com.revature.gambit;
 
-import java.sql.Timestamp;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-//import org.springframework.context.annotation.Bean;
-//
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import com.revature.gambit.services.BatchService;
-import com.revature.gambit.services.BatchServiceImpl;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -27,17 +15,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableEurekaClient
 public class Application {
-	
-	@Autowired
-	private static BatchService batchService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.revature.gambit")).paths(PathSelectors.any()).build();
 	}
-
 }
